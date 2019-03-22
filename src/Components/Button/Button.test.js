fa92c2bfbd16b5077316e779c6a3e2e1eab6136d
shakti97 <shaktisingh230797@gmail.com>
2019-03-23 00:00:06 +0530

    Refactor Button

--- src/Components/Button/Button.test.js
+++ src/Components/Button/Button.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import Button from './Button';
 
+let count
+
+const incrementCount = increment => {
+  count += increment
+}
 test('+1 Button works', () => {
     count = 0
     const { container } = render(
