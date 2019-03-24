81647bc8ae7d050b56df43df30e6277d63089abb
shakti97 <shaktisingh230797@gmail.com>
2019-03-24 22:49:22 +0530

    Another Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter()})
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount, render } from 'enzyme'
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// Enzyme.configure({ adapter: new Adapter()})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('App render a div',()=>{
+  const wrapper = shallow(<App/>)
+  expect(wrapper.type()).toBe('div')
+})
